466a1eef17da728a290b60d4ae07a57a43c08797
fschram <f.schramdejong@outlook.com>
2013-08-30 14:11:54 +0200

    formats code

--- springmvcapp/src/test/java/com/capgemini/model/PersonTest.java
+++ springmvcapp/src/test/java/com/capgemini/model/PersonTest.java
@@ -1,22 +1,14 @@
 package com.capgemini.model;
 
-import javax.validation.constraints.AssertTrue;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class PersonTest {
-	
-	@Test
-	public void shouldCreatePerson() {
-		Person p=new Person("samuel","maljaars");
-		Assert.assertEquals("samuel", p.getFirstName());
-		Assert.assertEquals("maljaars", p.getLastName());		
-	}
-	
-	
-	
-	
+
+    @Test
+    public void shouldCreatePerson() {
+        Person p = new Person("samuel", "maljaars");
+        Assert.assertEquals("samuel", p.getFirstName());
+        Assert.assertEquals("maljaars", p.getLastName());
+    }
 }
-
-
