fa3ca1e3e47056447068414cd0f2a01599b0061f
nandinichauhan <nandinirsc89@gmail.com>
2013-08-30 17:03:29 +0530

    Added curly braces.

--- springmvcapp/src/main/java/com/capgemini/model/Person.java
+++ springmvcapp/src/main/java/com/capgemini/model/Person.java
@@ -80,14 +80,17 @@ public class Person implements Serializable {
 		if (this == obj) {
 			return true;
 		}
-		if (obj == null)
+		if (obj == null){
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()){
 			return false;
+		}
 		Person other = (Person) obj;
 		if (firstName == null) {
-			if (other.firstName != null)
+			if (other.firstName != null){
 				return false;
+			}
 		} else if (!firstName.equals(other.firstName)){
 			return false;
 		}
@@ -95,13 +98,16 @@ public class Person implements Serializable {
 			if (other.id != null) {
 				return false;
 			}
-		} else if (!id.equals(other.id))
+		} else if (!id.equals(other.id)){
 			return false;
+		}
 		if (lastName == null) {
-			if (other.lastName != null)
+			if (other.lastName != null){
 				return false;
-		} else if (!lastName.equals(other.lastName))
+			}
+		} else if (!lastName.equals(other.lastName)){
 			return false;
+		}
 		return true;
 	}
 }
