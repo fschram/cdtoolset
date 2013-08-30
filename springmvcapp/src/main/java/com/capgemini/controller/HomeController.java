219f5a3df6e55ac293d306950494aa90646779ba
SMALJAAR <SMALJAAR@LBNL00038.corp.capgemini.com>
2013-08-30 13:44:06 +0200

    removed more tabulation chars

--- springmvcapp/src/main/java/com/capgemini/controller/HomeController.java
+++ springmvcapp/src/main/java/com/capgemini/controller/HomeController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    private static final Logger logger = LoggerFactory
-			.getLogger(HomeController.class);
+private static final Logger logger = LoggerFactory
+            .getLogger(HomeController.class);
 
 	/**
 	 * Selects the home page and populates the model with a message
