fd8f892aaaf074cfec8b8365266b819e77de8aec
JALEVY <JALEVY@LBNL06850.corp.capgemini.com>
2013-09-12 12:35:19 +0200

    ...

--- springmvcapp/src/main/java/com/capgemini/controller/HomeController.java
+++ springmvcapp/src/main/java/com/capgemini/controller/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-private static final Logger lOGGER = LoggerFactory
+private static final Logger LOGGER = LoggerFactory
     .getLogger(HomeController.class);
 
     /**
