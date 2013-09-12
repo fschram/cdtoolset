abfc7c0d3b434c9f67580a8f0cd14f979f496127
JALEVY <JALEVY@LBNL06850.corp.capgemini.com>
2013-09-12 11:48:46 +0200

    Tabs --> white spaces

--- springmvcapp/src/main/java/com/capgemini/dao/PersonDao.java
+++ springmvcapp/src/main/java/com/capgemini/dao/PersonDao.java
@@ -12,25 +12,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public class PersonDao {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 	
-	public Person find(Long id) {
-		return entityManager.find(Person.class, id);
-	}
+    public Person find(Long id) {
+        return entityManager.find(Person.class, id);
+    }
 	
-	@SuppressWarnings("unchecked")
-	public List<Person> getPeople() {
-		return entityManager.createQuery("select p from Person p").getResultList();
-	}
+    @SuppressWarnings("unchecked")
+    public List<Person> getPeople() {
+        return entityManager.createQuery("select p from Person p").getResultList();
+    }
 	
-	@Transactional
-	public Person save(Person person) {
-		if (person.getId() == null) {
-			entityManager.persist(person);
-			return person;
-		} else {
-			return entityManager.merge(person);
-		}		
-	}	
+    @Transactional
+    public Person save(Person person) {
+        if (person.getId() == null) {
+            entityManager.persist(person);
+            return person;
+        } else {
+            return entityManager.merge(person);
+        }		
+    }	
 }
